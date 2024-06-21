63284082f8b29348562d65d036795640976e7a61
Meriem El Mountasser <mrymgithub@gmail.com>
2024-06-21 10:15:54 +0100

    hmmm

--- srcs/requirements/mariadb/tools/mariadb.sh
+++ srcs/requirements/mariadb/tools/mariadb.sh
@@ -1,41 +1,15 @@
 #!/bin/sh
 
-# maria_USER='Meriem'
-# maria_PASSWORD='INCEPTION'
-# maria_ROOT_PASSWORD='Bullshit'
-# MARIADB_DATABASE='mery'
+service mariadb start
 
-# until mysqladmin ping -h localhost --silent; do
-#   sleep 1
-# done
+mysql <<EOF
+CREATE USER '$maria_USER'@'%' IDENTIFIED BY '$maria_PASSWORD';
+GRANT ALL PRIVILEGES ON *.* TO '$maria_USER'@'%' IDENTIFIED BY '$maria_PASSWORD';
+GRANT ALL PRIVILEGES ON *.* TO 'root'@'%' IDENTIFIED BY '$maria_ROOT_PASSWORD';
+FLUSH PRIVILEGES;
+CREATE DATABASE $maria_DATABASE;
+EOF
 
-# mariadbd <<EOF
-# FLUSH PRIVILEGES;
-# CREATE DATABASE $MARIADB_DATABASE;
+service mariadb stop
 
-# CREATE USER IF NOT EXISTS '${maria_USER}'@'%' IDENTIFIED BY '${maria_PASSWORD}';
-# GRANT ALL PRIVILEGES ON *.* TO '$maria_USER'@'%' IDENTIFIED BY '$maria_PASSWORD';
-# GRANT ALL PRIVILEGES ON *.* TO 'root'@'%' IDENTIFIED BY '$maria_ROOT_PASSWORD';
-# FLUSH PRIVILEGES;
-# EOF
-
-service mariadb start;
-
-
-mysql -e "CREATE DATABASE IF NOT EXISTS \`$MARIADB_DATABASE\`;"
-
-
-mysql -e "CREATE USER IF NOT EXISTS \`$maria_USER\`@'%' IDENTIFIED BY '$maria_PASSWORD';"
-
-
-mysql -e "GRANT ALL PRIVILEGES ON *.* TO '$maria_USER'@'%' IDENTIFIED BY '$maria_PASSWORD';"
-
-
-mysql -e "FLUSH PRIVILEGES;"
-
-kill $(cat /var/run/mysqld/mysqld.pid)
-
-mysqld
-
-# exec mariadbd
-
+exec mysqld
--- srcs/requirements/wordpress/tools/script.sh
+++ srcs/requirements/wordpress/tools/script.sh
@@ -1,33 +1,13 @@
-# # #!/bin/bash
-
-# # DB_NAME="mery"
-# # DB_USER="Meriem"
-# # DB_PASSWORD="INCEPTION"
-# # DB_HOST="172.17.0.2:3306"
-# # WP_CONFIG_PATH="/var/www/wordpress/wp-config.php"
-
-# # if [ -f "$WP_CONFIG_PATH" ]; then
-# #     echo "The file wp-config.php already exists."
-# # else
-# # sleep 5
-# #     wp config create --allow-root --dbname=$DB_NAME  --dbuser=$DB_USER  --dbpass=$DB_PASSWORD --dbhost=$DB_HOST  --path='/var/www/wordpress/'
-
-# # fi
-
-# # exec "$@"
-
-
 #!/bin/bash
-
 
 if [ -f "$WP_CONFIG_PATH" ]; then
     echo "The file wp-config.php already exists."
 else
     sleep 5
-    wp config create --allow-root --dbname=$DB_NAME  --dbuser=$DB_USER --dbpass=$DB_PASSWORD --dbhost=$DB_HOST --path='/var/www/wordpress/'
-    wp core install --url="localhost" --title="Inception" --admin_user="$DB_USER" \
-	--admin_password="$DB_PASSWORD" --admin_email="meriemelmountasser3@gmail.com" --skip-email --allow-root
-	wp user create $ADMIN_USER hia@gmail.com --role=author --user_pass=$ADMIN_PWD --allow-root
+    wp config create --allow-root --dbname=$maria_DATABASE  --dbuser=$maria_USER --dbpass=$maria_PASSWORD --dbhost=$DB_HOST --path='/var/www/wordpress/'
+    wp core install --url="localhost" --title="mel-moun" --admin_user="$ADMIN_USER" \
+	--admin_password="$ADMIN_PWD" --admin_email="ouioui@gmail.com" --skip-email --allow-root
+	wp user create $maria_USER nonnon@gmail.com --role=author --user_pass=$maria_PASSWORD --allow-root
 fi
     chown -R www-data:www-data /var/www/html
 
