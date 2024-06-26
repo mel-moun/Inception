ae7955903fdd65245c1a58e57c4449a3af6683fa
Meriem El Mountasser <mrymgithub@gmail.com>
2024-06-26 13:07:56 +0100

    added things f .env and script

--- srcs/requirements/wordpress/tools/script.sh
+++ srcs/requirements/wordpress/tools/script.sh
@@ -8,8 +8,8 @@ if [ -f "$WP_CONFIG_PATH" ]; then
 else
     sleep 5
     wp config create --allow-root --dbname=$maria_DATABASE  --dbuser=$maria_USER --dbpass=$maria_PASSWORD --dbhost=$DB_HOST --path='/var/www/wordpress/'
-    wp core install --url="mel-moun.42.fr" --title="mel-moun" --admin_user="$ADMIN_USER" \
-	--admin_password="$ADMIN_PWD" --admin_email="ouioui@gmail.com" --skip-email --allow-root
+    wp core install --url=$URL --title=$TITLE --admin_user=$ADMIN_USER \
+	--admin_password="$ADMIN_PWD" --admin_email=$ADMIN_EMAIL --skip-email --allow-root
 	wp user create $maria_USER nonnon@gmail.com --role=author --user_pass=$maria_PASSWORD --allow-root
 fi
     chown -R www-data:www-data /var/www/html
